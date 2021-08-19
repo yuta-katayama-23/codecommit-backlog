ef2187db7954d65d803d878ee00a9f02ab99dca2
Yuta Katayama <yutapilafu@gmail.com>
2021-08-19 23:01:16 +0900

    Backlogにコメントを記載できるようにするまで完成

--- README.md
+++ README.md
@@ -23,6 +23,8 @@ https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-codecommit
 
 ### Cloud Watch Events の Event オブジェクトの中身
 
+Code Commit への push の際に、Cloud Watch Events としては以下の`referenceUpdated`というタイプのイベントが発火する<br>そ Event オブジェクトの中身としては以下
+
 ```json
 {
   "version": "0",
@@ -50,3 +52,31 @@ https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-codecommit
 ```
 
 https://docs.aws.amazon.com/ja_jp/codecommit/latest/userguide/monitoring-events.html#referenceUpdated
+
+### Backlog API でコミットハッシュを issue(チケット)に記載
+
+コミットハッシュを issue に紐づける処理のロジックとしては以下<br>（環境変数 BACKLOG_PJ_ID で Backlog から取得する issue の対象プロジェクトを projectId で絞り込めるように、BACKLOG_ISSUE_TYPE_NAME で プロジェクト内の issue を取得する際に対象を issue type で絞り込めるように実装した）
+
+1. **BACKLOG_PJ_ID=ALL の場合**<br>Backlog の PJ 全てを取得<br>**BACKLOG_PJ_ID=ALL 以外の場合**<br>BACKLOG_PJ_ID の projectId のみ利用
+1. **BACKLOG_ISSUE_TYPE_NAME が未指定の場合**<br>projectId で Backlog の プロジェクト内の issue データを全件取得<br>**BACKLOG_ISSUE_TYPE_NAME が指定されている場合**<br>BACKLOG_ISSUE_TYPE_NAME の issue type に合致する issue だけを抽出<br>
+1. コミットメッセージに書かれている issue との紐づけ文言から、commit に関係する issue の issueId を抽出
+1. issuesId を用いて Backlog にコメントを記載
+
+利用した Backlog の API は以下<br>
+※今回は[認証](https://developer.nulab.com/ja/docs/backlog/auth)は API KEY で行っている
+
+- [プロジェクト一覧の取得](https://developer.nulab.com/ja/docs/backlog/api/2/get-project-list)
+
+- [課題一覧の取得](https://developer.nulab.com/ja/docs/backlog/api/2/get-issue-list)
+- [課題コメントの追加](https://developer.nulab.com/ja/docs/backlog/api/2/add-comment)
+
+axios で`Content-Type:application/x-www-form-urlencoded `で POST するには[Query string](https://github.com/axios/axios#query-string)を使えばいい
+
+## トラブルシューティング
+
+### forEach は非同期処理で利用不可
+
+非同期の処理は`for-of`とかを使う
+※forEach は同期関数を期待する<br>https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+
+ちなみに、forEach の中身は`while`になっている
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const { CodeCommitClient, GetCommitCommand } = require("@aws-sdk/client-codecommit");
+const axios = require("axios").default;
+const querystring = require('querystring');
 
-const cinfig = { region: "ap-northeast-1" };
-const client = new CodeCommitClient(cinfig);
+const regex = /^\[#(.*)]/
+const config = {
+    baseURL: `https://${process.env.BACKLOG_DOMAIN}.backlog.com/api/v2`
+};
+const client = new CodeCommitClient({ region: process.env.AWS_REGION });
 
 exports.handler = async (event) => {
     const input = {
@@ -9,7 +14,68 @@ exports.handler = async (event) => {
         repositoryName: event.detail.repositoryName
     }
     const command = new GetCommitCommand(input);
-
     const response = await client.send(command);
-    return response;
+    const commitMsg = response.commit.message;
+    const ticketSearchKey = commitMsg.match(regex)[1];
+
+    /* Backlogから種別バグを取得 */
+    try {
+        // BacklogからprojectIdを取得
+        let pjIds = [];
+        if (process.env.BACKLOG_PJ_ID === "ALL") {
+            const pjs = await axios.get(`/projects?apiKey=${process.env.BACKLOG_API_KEY}`, config);
+            pjs.data.forEach(pj => pjIds.push(pj.id));
+        } else pjIds.push(process.env.BACKLOG_PJ_ID);
+
+        // projectIdでそれぞれissueを取得（issue typeの指定があればそれでフィルタリング）
+        let issues = [];
+        for (const pjId of pjIds) {
+            const fectedIssues = await axios.get(`/issues?apiKey=${process.env.BACKLOG_API_KEY}&projectId[]=${pjId}`, config);
+            if (process.env.BACKLOG_ISSUE_TYPE_NAME) {
+                fectedIssues.data
+                    .filter(issue => issue.issueType.name === process.env.BACKLOG_ISSUE_TYPE_NAME)
+                    .map(issue => reObj(issue))
+                    .forEach(issue => issues.push(issue));
+            } else fectedIssues.data.map(issue => reObj(issue)).forEach(issue => issues.push(issue));
+        }
+
+        // issueの中からコミットメッセージのissue特定キーワードを含むものを抽出
+        const issueId = issues.filter(issue => issue.summary.includes(ticketSearchKey))[0].id;
+
+        // issueにコメントを追加
+        const message = "Commit Message";
+        const hash = "Code Commit CommitHash";
+        const link = `[${input.commitId}](https://${process.env.AWS_REGION}.console.aws.amazon.com/codesuite/codecommit/repositories/${input.repositoryName}/commit/${input.commitId}?region=${process.env.AWS_REGION})`;
+        const data = querystring.stringify({
+            content: `## ${message}\n${commitMsg}\n## ${hash}\n${link}`
+        });
+        const result = await axios.post(`/issues/${issueId}/comments?apiKey=${process.env.BACKLOG_API_KEY}`, data, config);
+
+        return {
+            status: result.status,
+            statusText: result.statusText,
+            id: result.data.id,
+            content: result.data.content
+        };
+    } catch (error) {
+        return errorHandler(error);
+    }
+}
+
+const errorHandler = (error) => {
+    const obj = {};
+    if (error.response) {
+        obj["status"] = error.response.status;
+        obj["statusText"] = error.response.statusText;
+        obj["data"] = error.response.data;
+    }
+    obj["message"] = error.message;
+    return obj;
+}
+
+const reObj = (issue) => {
+    let rObj = {};
+    rObj["id"] = issue.id;
+    rObj["summary"] = issue.summary;
+    return rObj;
 }
